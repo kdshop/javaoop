26c5908b4fb2e771b7275d46beb738b6f4edc432
Konrad <kontakt@konradalbrecht.pl>
2019-10-15 00:19:42 +0200

    Ugrh ;/

--- Laborki/src/praca_domowa/PracaDomowa1.java
+++ Laborki/src/praca_domowa/PracaDomowa1.java
@@ -5,22 +5,42 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.groupingBy;
 
 public class PracaDomowa1 {
 
     public static void main(String[] args) throws IOException {
-        readCSVFile().values().forEach(System.out::println);
+        Dane dane = readCSVFile();
+
+        dane.getData().forEach(strings -> strings.forEach(System.out::println));
     }
 
-    private static Map<Stream<String>, List<String>> readCSVFile() throws IOException {
+    private static Dane readCSVFile() throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader("2019-10-11_akcje.csv"))) {
-            return br.lines()
+            String[] header = br.readLine().split(",");
+
+            List<ArrayList<String>> dane = br.lines()
+                .map(s -> new ArrayList<>(Arrays.asList(s.split(","))))
                 .collect(
-                    groupingBy(String::lines, LinkedHashMap::new, Collectors.toList())
+                    Collectors.toList()
                 );
+
+            return new Dane(header, dane);
         }
+    }
+}
+
+class Dane {
+    private String[] header;
+    private List<ArrayList<String>> data;
+
+    Dane(String[] header, List<ArrayList<String>> data) {
+        this.header = header;
+        this.data = data;
+    }
+    String[] getHeader() {
+        return this.header;
+    }
+    List<ArrayList<String>> getData() {
+        return this.data;
     }
 }
